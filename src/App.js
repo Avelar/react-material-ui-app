6b7b3f905a07238890237e1721ef684190deed7c
avelar <fortunato.avelar@gmail.com>
2020-06-07 10:11:09 -0300

    init project with material ui; added theme file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*
+          <img src={logo} className="App-logo" alt="logo" />
+        */}
+        Header
       </header>
+      <Container maxWidth="sm">
+      Conteiner
+
+      <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+
+
+        <Button variant="contained" color="secondary">
+          Continuar
+        </Button>
+      </Container>
     </div>
   );
 }
-
 export default App;
